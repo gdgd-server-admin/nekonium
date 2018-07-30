b90dd3403ddd99ad28b38f6bd5140edb720a638a
lin <admin@gdgd.jp.net>
2018-07-30 22:14:29 +0900

    #2 saveViewModeはトグルスイッチが押されたときに呼ばれるので呼び出し元の情報をもとに設定を保存する形に変更した。 トグルスイッチに名前を付けたのでキー名を変更した。 設定読み出しのif文をしんぷりふぁいした。

--- ViewMode.js
+++ ViewMode.js
@@ -2,11 +2,8 @@ var Observable = require("FuseJS/Observable");
 var api = require("Assets/js/api");
 
 var NightMode = Observable(false);
-var MoveLeft = Observable(false);
-var MoveUp = Observable(false);
 var ImageViewer = Observable(false);
 var LocalNotify = Observable(false);
-var AlignChange = Observable(false);
 var ShiitakeX = Observable("80%");
 var ShiitakeY = Observable("85%");
 var MovableShiitake = Observable(false);
@@ -25,12 +22,20 @@ var _logged = false;
 
 function toggleNightMode(){
 	NightMode.value = !NightMode.value;
-	saveViewMode();
+	var args = {
+		"sender": "NightModeEnabled",
+		"value": NightMode.value
+	}
+	saveViewMode(args);
 }
 
 function toggleLocalNotify(){
 	LocalNotify.value = !LocalNotify.value;
-	saveViewMode();
+	var args = {
+		"sender": "LocalNotiEnabled",
+		"value": LocalNotify.value
+	}
+	saveViewMode(args);
 }
 
 function moveShiitake(args){
@@ -81,98 +86,37 @@ function toggleShiitakeRight(){
 	saveViewMode();
 }
 
-function saveViewMode(){
-	console.log("save viewmode");
-	//NightMode
-	if(NightMode.value){
-		localStorage.setItem('NightMode', true);
+function saveViewMode(args){
+
+	console.log("Turn " + args.sender + " " + args.value);
+
+	// args.senderをlocalStorageのキーに
+	// args.valueを設定内容に
+	if(args.value){
+		localStorage.setItem(args.sender, true);
 	}else{
-		localStorage.removeItem('NightMode');
+		localStorage.removeItem(args.sender);
 	}
 
-	//MoveLeft
-	if(MoveLeft.value){
-		localStorage.setItem("MoveLeft",true);
-	}else{
-		localStorage.removeItem("MoveLeft");
-	}
-
-	//MoveUp
-	if(MoveUp.value){
-		localStorage.setItem("MoveUp",true);
-	}else{
-		localStorage.removeItem("MoveUp");
-	}
-
-	//ImageViewer
-	if(ImageViewer.value){
-		localStorage.setItem('ImageViewer', true);
-	}else{
-		localStorage.removeItem('ImageViewer');
-	}
-
-	//LocalNotify
-	if(LocalNotify.value){
-		localStorage.setItem('LocalNotify', true);
-	}else{
-		localStorage.removeItem('LocalNotify');
-	}
-
-	//AlignChange
-	if(AlignChange.value){
-		localStorage.setItem('AlignChange', true);
-	}else{
-		localStorage.removeItem('AlignChange');
-	}
-
-	//MovableShiitake
-	if(MovableShiitake.value){
-		localStorage.setItem('MovableShiitake', true);
-	}else{
-		localStorage.removeItem('MovableShiitake');
-	}
-
-	//ShiitakeAlign
+	// ShiitakeAlign
+	// こいつはどうするかな
 	localStorage.setItem('ShiitakeAlign', ShiitakeAlign.value);
 }
 
 function getInitialDataFromLocalStorage(){
-	console.log("get viewmode");
-	if(localStorage.getItem("NightMode") != undefined){
-		NightMode.value = true;
-	}else{
-		NightMode.value = false;
-	}
 
-	if(localStorage.getItem("MoveLeft") != undefined){
-		MoveLeft.value = true;
-	}else{
-		MoveLeft.value = false;
-	}
+	// localStorageに特定のキーの値がセットされているかどうかの確認
 
-	if(localStorage.getItem("MoveUp") != undefined){
-		MoveUp.value = true;
-	}else{
-		MoveUp.value = false;
-	}
+	NightMode.value = (localStorage.getItem("NightModeEnabled") != undefined);
 
-	if(localStorage.getItem("ImageViewer") != undefined){
-		ImageViewer.value = true;
-	}else{
-		ImageViewer.value = false;
-	}
+	ChatEnabled.value = (localStorage.getItem("PleromaChatEnabled") != undefined);
 
-	if(localStorage.getItem("LocalNotify") != undefined){
-		LocalNotify.value = true;
-	}else{
-		LocalNotify.value = false;
-	}
+	LocalNotify.value = (localStorage.getItem("LocalNotifyEnabled") != undefined);
 
-	if(localStorage.getItem("AlignChange") != undefined){
-		AlignChange.value = true;
-	}else{
-		AlignChange.value = false;
-	}
+	ImageViewer.value = (localStorage.getItem("ImageViewerEnabled") != undefined);
+
+	MovableShiitake.value = (localStorage.getItem("MovableShiitakeEnabled") != undefined);
+
 
 	if(localStorage.getItem("ShiitakeX") != undefined){
 		ShiitakeX.value = localStorage.getItem("ShiitakeX");
@@ -180,12 +124,6 @@ function getInitialDataFromLocalStorage(){
 
 	if(localStorage.getItem("ShiitakeY") != undefined){
 		ShiitakeY.value = localStorage.getItem("ShiitakeY");
-	}
-
-	if(localStorage.getItem("MovableShiitake") != undefined){
-		MovableShiitake.value = true;
-	}else{
-		MovableShiitake.value = false;
 	}
 
 	if(localStorage.getItem("ShiitakeAlign") != undefined){
@@ -208,24 +146,17 @@ function getInitialDataFromLocalStorage(){
 		VTagText.value = localStorage.getItem("VTagText");
 	}
 
-	if(localStorage.getItem("ChatEnabled") != undefined){
-		ChatEnabled.value = true;
-	}else{
-		ChatEnabled.value = false;
-	}
+
 }
 
 module.exports = {
 	getInitialDataFromLocalStorage:getInitialDataFromLocalStorage,
 	saveViewMode:saveViewMode,
-    NightMode:NightMode,
-	MoveLeft:MoveLeft,
-	MoveUp:MoveUp,
+  NightMode:NightMode,
 	toggleNightMode:toggleNightMode,
 	ImageViewer:ImageViewer,
 	LocalNotify:LocalNotify,
 	toggleLocalNotify:toggleLocalNotify,
-	AlignChange:AlignChange,
 	ShiitakeX:ShiitakeX,
 	ShiitakeY:ShiitakeY,
 	moveShiitake:moveShiitake,
